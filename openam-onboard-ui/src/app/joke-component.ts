88038280b341951f8a761cd22db94082edc1e754
shyamsivaraman <shyamsivaraman@gmail.com>
2018-09-09 11:32:33 +0530

    Simple list-component without @Input usage

--- openam-onboard-ui/src/app/joke-component.ts
+++ openam-onboard-ui/src/app/joke-component.ts
@@ -1,18 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component(
     {
         selector : 'joke',
-        template : `<h4 hidden>{{setup}}</h4><h5>{{punchline}}</h5>`
+        template : `<h4 class="card-title">{{data.setup}}</h4>
+        <p class="card-text" [hidden]="data.hide">{{data.punchline}}</p>
+        <div style='display:inline-block;color:white;text-align:right;'>
+            <a class="btn btn-primary" (click)="data.hide = !data.hide">Tell Me</a>
+        </div>`
     }
 )
-
 export class JokeComponent {
-    setup : string;
-    punchline : string;
+    @Input('joke')
+    data: Joke;
+}
 
-    constructor() {
-        this.setup = "What did the cheese say when it looked in the mirror?";
-        this.punchline = "Hello";
+export class Joke {
+    public setup: String;
+    public punchline: String;
+    public hide: boolean;
+
+    constructor(setup: String, p:String) {
+        this.setup = setup;
+        this.punchline = p;
+        this.hide = true;
     }
 }
--- openam-onboard-ui/src/app/joke-list-component.ts
+++ openam-onboard-ui/src/app/joke-list-component.ts
@@ -1,33 +1,22 @@
 import { Component } from "@angular/core"
+import { Joke } from "app/joke-component";
 
 @Component ( {
     selector: 'joke-list',
     template: `
-        <div class='card card-block' *ngFor = "let joke of jokes" 
+        <div class='card card-block' *ngFor = "let j of jokes" 
             style='padding:10px;margin:10px;'>
-            <h4 class="card-title">{{joke.setup}}</h4>
-            <p class="card-text" [hidden]="joke.hide">{{joke.punchline}}</p>
-            <div style='display:inline-block;color:white;text-align:right;'>
-                <a class="btn btn-primary" (click)="joke.hide = !joke.hide">Tell Me</a>
-            </div>
+            <joke [joke]="j"></joke>
         </div>
     `
 } )
 export class JokeListComponent {
-    jokes: Object[];
+    jokes: Joke[];
 
     constructor() {
         this.jokes = [
-            {
-                setup: "Joke 1",
-                punchline: "P1",
-                hide: true
-            },
-            {
-                setup: "Joke 2",
-                punchline: "P2",
-                hide: true
-            }
+            new Joke("Joke 1","P1"),
+            new Joke("Joke 2", "P2")
         ];
     }
 }
